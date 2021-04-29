d180d7110932ad353b3d80daa9bd095b1991ad43
Baker <eab6863@gmail.com>
2021-04-29 15:16:04 -0400

    Doing stuff

---
+++ app.js
@@ -0,0 +1,20 @@
+const {
+  App
+} = require('@slack/bolt');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
