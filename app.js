467053aa45fb229edf7e72a9eb74d26122c51870
Baker <eab6863@gmail.com>
2021-04-29 16:53:40 -0400

    added hello

--- app.js
+++ app.js
@@ -13,6 +13,31 @@ const app = new App({
   appToken: process.env.APP_TOKEN,
 });
 
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
+  });
+});
+
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
